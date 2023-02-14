4f162482899137533867b578d1fffc1970aff6aa
Alik_Avizha <aleksandravizha@icloud.com>
2023-02-14 14:00:06 +0300

    change App.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <div>One Div</div>
+            <div className="App">
+                <ul>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>JS</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
