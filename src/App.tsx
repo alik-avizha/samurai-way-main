d5c0aa44eb43119e85a1e51c754ec9f7a549ebaa
Alik_Avizha <aleksandravizha@icloud.com>
2023-02-14 14:56:41 +0300

    change App.tsx and add new file Header.tsx and Techlogies.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 const App = () => {
     return (
         <div>
-            <div>One Div</div>
-            <div className="App">
-                <ul>
-                    <li>CSS</li>
-                    <li>HTML</li>
-                    <li>JS</li>
-                    <li>React</li>
-                </ul>
-            </div>
+            <Header/>
+            <Technologies/>
         </div>
     );
 }
